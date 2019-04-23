4c2eba93f6d733348c7a5bca686f241d3f46c9c8
Ivan Fuentes <ivanfuentesglez@gmail.com>
2019-04-23 15:49:11 -0500

    Animación V2

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-  	center:true,
-  	items:1,
-  	loop:true,
-  	autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    animateIn:'fadeIn',
-    animateOut:'fadeOut',
-    smartSpeed: 2000
-  });
+  // $(".owl-carousel").owlCarousel({
+  // 	center:true,
+  // 	items:1,
+  // 	loop:true,
+  // 	autoplay:true,
+  //   autoplayTimeout:5000,
+  //   autoplayHoverPause:true,
+  //   animateIn:'fadeIn',
+  //   animateOut:'fadeOut',
+  //   smartSpeed: 2000
+  // });
+   var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    
+    showNextQuote();
 });
